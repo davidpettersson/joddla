388f030267e23d2c8e6a23d4858ef6a71767f91d
David Pettersson <david.pettersson@lund.se>
2013-08-07 15:07:23 +0200

    Inverted arcs

--- dxf.py
+++ dxf.py
@@ -12,16 +12,16 @@ LAYER_ANNO = 'Annotations'
 
 
 def write_dxf(filename, points, lines, arcs):
-    dwg = ezdxf.new(dxfversion='AC1024') # AutoCAD 2010
+    dwg = ezdxf.new(dxfversion='AC1024')  # AutoCAD 2010
     layout = dwg.modelspace()
     for line in lines:
         l = layout.add_line((line[0], line[1]), (line[2], line[3]))
         l.set_dxf_attrib('layer', LAYER_GEOM)
     for arc in arcs:
         if arc[6]:
-            a = layout.add_arc((arc[0], arc[1]), arc[2], degrees(arc[3]), degrees(arc[4]))
-        else:
             a = layout.add_arc((arc[0], arc[1]), arc[2], degrees(arc[4]), degrees(arc[3]))
+        else:
+            a = layout.add_arc((arc[0], arc[1]), arc[2], degrees(arc[3]), degrees(arc[4]))
         a.set_dxf_attrib('layer', LAYER_GEOM)
     for point in points:
         s = point.name
