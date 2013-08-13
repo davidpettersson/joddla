02b141a42d173c3415badb44fdcee982ba405388
David Pettersson <david@shebang.nu>
2013-08-13 15:37:48 +0200

    Added license headers.

--- util.py
+++ util.py
@@ -1,9 +1,24 @@
 #
 # util.py
 #
+# Copyright (C) 2013 City of Lund (Lunds kommun)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 
 from pprint import pprint
 from math import sqrt
+
 
 def bounding_box(points):
     bbox = {
@@ -23,5 +38,6 @@ def bounding_box(points):
     pprint(bbox)
     return bbox
 
+
 def distance(x0, y0, x1, y1):
-    return sqrt((y1-y0)**2 + (x1-x0)**2)
+    return sqrt((y1 - y0) ** 2 + (x1 - x0) ** 2)
