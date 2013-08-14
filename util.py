68a5380c122a8f8ee51e74eb9e4a3d7798cbb69e
David Pettersson <david.pettersson@lund.se>
2013-08-14 11:57:36 +0200

    Replaced usage of old bounding_box with new BoundingBox class.

--- util.py
+++ util.py
@@ -13,27 +13,7 @@
 # limitations under the License.
 
 
-from pprint import pprint
 from math import sqrt
-
-
-def bounding_box(points):
-    bbox = {
-        'min_x': 1e10,
-        'max_x': 0.0,
-        'min_y': 1e10,
-        'max_y': 0.0,
-    }
-    for point in points:
-        bbox['min_x'] = min(bbox['min_x'], point.x)
-        bbox['min_y'] = min(bbox['min_y'], point.y)
-        bbox['max_x'] = max(bbox['max_x'], point.x)
-        bbox['max_y'] = max(bbox['max_y'], point.y)
-    bbox['width'] = bbox['max_x'] - bbox['min_x']
-    bbox['height'] = bbox['max_y'] - bbox['min_y']
-    print 'Bounding box:'
-    pprint(bbox)
-    return bbox
 
 
 def distance(x0, y0, x1, y1):
