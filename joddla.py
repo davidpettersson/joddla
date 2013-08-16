ddba938ab732c69a54bd69721fde916b01dbe50b
David Pettersson <david@shebang.nu>
2013-08-16 15:54:54 +0200

    Factored problem solving alg module.

--- joddla.py
+++ joddla.py
@@ -14,115 +14,12 @@
 
 
 from pprint import pprint
-from math import sqrt, acos, pi
 import sys
-from numpy import array
-from joddla.alg import find_slopes, find_line_segments, formulate_problems
+from joddla.alg import find_slopes, find_line_segments, formulate_problems, solve_problem
 from joddla.model import ArcSegment, LineSegment
 from joddla.render import draw_screen
 from joddla.jobxml import load
 from joddla.dxf import dump
-
-
-# TODO: Need to decide suitable threshold
-LINE_SNAP_THRESHOLD = 0.95
-
-
-def _distance(x0, y0, x1, y1):
-    return sqrt((y1 - y0) ** 2 + (x1 - x0) ** 2)
-
-
-def _quadrant_offset(p, x, y):
-    if p.x() >= x:
-        if p.y() >= y:
-            return 0, 1
-        else:
-            return 2.0 * pi, -1
-    else:
-        if p.y() >= y:
-            return pi, -1
-        else:
-            return pi, 1
-
-
-def solve(problem):
-    print '----solving----'
-
-    d = _distance(problem.a.x(), problem.a.y(), problem.b.x(), problem.b.y())
-    print 'Distance between points:', d
-
-    # TODO: Lots of magic constants
-    step_size = d / 1000
-    step_extension = d * 100
-    step_start = -step_extension
-    step_stop = step_extension
-    step_distance = step_stop - step_start
-    step_count = step_distance / step_size
-    steps = [(step_start + step_size * k) for k in range(int(step_count))]
-
-    # TODO: This is an extremely naive numerical search. Replace with an
-    #   analytical solution (probably some derivative stuff).
-
-    best_error = 100000000
-    best_x = 0.0
-    best_y = 0.0
-    best_radius = 0.0
-    improvements = 0
-
-    for step in steps:
-        x = problem.c[0] + problem.s.x() * step
-        y = problem.c[1] + problem.s.y() * step
-        radius = _distance(problem.a.x(), problem.a.y(), x, y)
-
-        # Calculate tangent for a
-        dx = (problem.a.x() - x)
-        dy = (problem.a.y() - y)
-        a_k = -dx / dy
-        error_a = a_k - problem.p.k()
-
-        # Calculate tangent for b
-        dx = (problem.b.x() - x)
-        dy = (problem.b.y() - y)
-        b_k = -dx / dy
-        error_b = b_k - problem.q.k()
-
-        # Figure out the error
-        error = sqrt(error_a ** 2 + error_b ** 2)
-
-        # Pick the lowest error
-        if error < best_error:
-            best_error = error
-            best_x = x
-            best_y = y
-            best_radius = radius
-            improvements += 1
-
-    print 'Step:', step_size
-    print 'Improvements:', improvements
-    print 'Error:', best_error
-    print 'Radius:', best_radius
-    print 'Extension', step_extension
-
-    if (best_radius / step_extension) > LINE_SNAP_THRESHOLD:
-        return LineSegment(problem.a, problem.b)
-
-    o, m = _quadrant_offset(problem.a, best_x, best_y)
-    angle_a = o + m * acos(abs(problem.a.x() - best_x) / best_radius)
-
-    o, m = _quadrant_offset(problem.b, best_x, best_y)
-    angle_b = o + m * acos(abs(problem.b.x() - best_x) / best_radius)
-
-    if angle_a > angle_b:
-        angle_a, angle_b = angle_b, angle_a
-
-    dz = problem.b.z() - problem.a.z()
-    best_z = problem.a.z() + dz / 2.0
-
-    return ArcSegment(
-        array([best_x, best_y, best_z]),
-        best_radius,
-        angle_a,
-        angle_b)
 
 
 def main(filename, render):
@@ -150,7 +47,7 @@ def main(filename, render):
     pprint(problems)
 
     print '--- SOLUTIONS'
-    solutions = map(solve, problems)
+    solutions = map(solve_problem, problems)
     pprint(solutions)
 
     arc_segments = filter(lambda s: isinstance(s, ArcSegment), solutions)
